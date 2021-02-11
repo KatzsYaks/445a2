10cb7881809eecd2ab84ba2ebf0a07eaad8f1075
KatzsYaks <77358536+KatzsYaks@users.noreply.github.com>
2021-02-10 23:39:57 -0800

    First Commit
    Added Unity Project to GitHub

---
+++ house thingy/Assets/Scripts/DoorCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCollision : MonoBehaviour
+{
+
+    public AudioSource source;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col){
+        source.Play();
+    }
+}
